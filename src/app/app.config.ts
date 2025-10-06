6ff4ad470cfd420d04d80dba58ea821b2428e42e
Abel Serra <aj_serra@hotmail.com>
2025-10-06 21:45:17 +0200

    vercel configuracion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
@@ -12,8 +11,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([authInterceptor]),
-      withFetch()  // ← AGREGAR ESTO para mejor rendimiento con SSR
-    ),
-    provideClientHydration()
+      withFetch()
+    )
+    // Comentar o eliminar provideClientHydration()
   ]
 };
